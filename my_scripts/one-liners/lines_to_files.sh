3b422da3c69520a6a2de0620e455dd1286a989a3
Joshua Foreman <9174473+skttlock@users.noreply.github.com>
2024-12-06 11:11:53 -0800

    BAM!

--- my_scripts/one-liners/lines_to_files.sh
+++ my_scripts/one-liners/lines_to_files.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Script Name: line_to_file_extractor.sh
-# Version: 1.1
+# Version: 1.2
 # Author: Your Name
 # Date: 2024-12-06
-# Description: This awk command extracts the first, second, third, and last words from each line of input.txt to create filenames, then writes each line to its respective file.
+# Description: This awk command extracts the first, second, third, and last words from each line of _input.txt, removes any special characters, creates filenames, and writes each line to its respective file.
 # Generated with the help of Microsoft Copilot
 
-awk '{filename = $1 "_" $2 "_" $3 "_" $NF ".txt"; print > filename}' input.txt
+awk '{gsub(/[^a-zA-Z0-9]/, "", $1); gsub(/[^a-zA-Z0-9]/, "", $2); gsub(/[^a-zA-Z0-9]/, "", $3); gsub(/[^a-zA-Z0-9]/, "", $NF); filename = $1 "_" $2 "_" $3 "_" $NF ".txt"; print > filename}' _input.txt
+
