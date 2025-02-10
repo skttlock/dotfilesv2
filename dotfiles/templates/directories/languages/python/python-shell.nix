aba72da932779ddffed34101fd6ed2fae1e53d7a
Joshua Foreman <9174473+skttlock@users.noreply.github.com>
2025-02-10 15:17:53 -0800

    add pip2nix to python shell instructions

--- dotfiles/templates/directories/languages/python/python-shell.nix
+++ dotfiles/templates/directories/languages/python/python-shell.nix
@@ -1,15 +1,26 @@
 let
   # pin version to 24.11
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import nixpkgs {
+    config = {};
+    overlays = [];
+  };
 
+  # Use pip2nix to package any pip package for nix (when not available on NixPkgs)
+
+  # packageOverrides = pkgs.callPackage ./python-packages.nix {};
+  # python3 = pkgs.python3.override {inherit packageOverrides;};
   devPackages = with pkgs; [
-      # install packages
-      (python3.withPackages (python-pkgs: with python-pkgs; [
+    (python3.withPackages (pypkgs:
+      with pypkgs; [
+        # Then install them here
       ]))
-    ];
+  ];
   aliases = [
-    { alias = "gs"; command = "git status"; }
+    {
+      alias = "py";
+      command = "python";
+    }
   ];
 in
   pkgs.mkShell {
@@ -17,21 +28,22 @@ in
     SHELLTYPE = "dev";
 
     shellHook = ''
-      echo "------------------------";
-      echo "Entered $SHELLTYPE environment in:";
-      pwd;
-      echo "Using these packages:";
-      for pkg in ${pkgs.lib.concatStringsSep " " (map (pkg: "${pkg.pname}-${pkg.version}") devPackages)}; do
-        echo "Package: $pkg"
-      done
-      echo "";
-      echo "           ==           ";
-      echo "";
-      echo "With these aliases:";
-    ${builtins.concatStringsSep "\n" (map (def: ''
-      alias ${def.alias}='${def.command}'
-      echo "Alias set: ${def.alias} -> ${def.command}"
-    '') aliases)}
-      echo "------------------------";
-      '';
+        echo "------------------------";
+        echo "Entered $SHELLTYPE environment in:";
+        pwd;
+        echo "Using these packages:";
+        for pkg in ${pkgs.lib.concatStringsSep " " (map (pkg: "${pkg.name}") devPackages)}; do
+          echo "Package: $pkg"
+        done
+        echo "";
+        echo "           ==           ";
+        echo "";
+        echo "With these aliases:";
+      ${builtins.concatStringsSep "\n" (map (def: ''
+          alias ${def.alias}='${def.command}'
+          echo "Alias set: ${def.alias} -> ${def.command}"
+        '')
+        aliases)}
+        echo "------------------------";
+    '';
   }
